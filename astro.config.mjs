17120f2d1793f8794ea90fcf5cd4aeaf6aed0f03
Paket Wisata Dieng <wikipedieng@gmail.com>
2025-07-09 20:40:15 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,6 @@ export default defineConfig({
         react(),
         sitemap(),    // <--- aktifkan plugin sitemap
     ],
-    adapter: netlify(),
+// adapter: netlify(), // matikan dulu
+
 });
