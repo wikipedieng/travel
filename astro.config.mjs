e27b4f8de5dcf5907fd3aac7f72d9e5bbe3bf7d1
Paket Wisata Dieng <wikipedieng@gmail.com>
2025-07-09 20:17:49 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; // <--- tambahkan ini!
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://dieng.travel', // <--- wajib isi domain utama di sini!
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
-    adapter: netlify()
+    integrations: [
+        react(),
+        sitemap(),    // <--- aktifkan plugin sitemap
+    ],
+    adapter: netlify(),
 });
